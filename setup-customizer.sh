e0e21a2551a43c90548f6f1964c4c4bd7395f7e9
Lu Ji <lu.ji1@me.com>
2021-12-09 22:24:07 +0000

    fix

--- setup-customizer.sh
+++ setup-customizer.sh
@@ -3,11 +3,16 @@
 # TODO: 
 # ** Uncomment this if this is a WordPress theme project ** 
 THEME_NAME="my-theme" # use current directory name as theme name by default 
-ln -s $PROJECT_ROOT $WP_ROOT/wp-content/themes/$THEME_NAME
+if [ -f $WP_ROOT/wp-content/themes/$THEME_NAME ]; then
+  ln -s $PROJECT_ROOT $WP_ROOT/wp-content/themes/$THEME_NAME
+fi 
 
 # ** Uncomment this if this is a WordPress plugin project ** 
 # PLUGIN_NAME="my-plugin" # use current directory name as plugin name by default 
-# ln -s $PROJECT_ROOT $WP_ROOT/wp-content/plugins/$PLUGIN_NAME
+# if [ -f $WP_ROOT/wp-content/plugins/$PLUGIN_NAME ]; then
+#   ln -s $PROJECT_ROOT $WP_ROOT/wp-content/plugins/$PLUGIN_NAME
+# fi 
+
 
 # TODO: 
 # You can also build a multi-project (multi plugins + themes) project when symlinks are configured right. 
